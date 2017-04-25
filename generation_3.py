6815a782a6baa58331be50b0ce10208ffe7b0ca9
ViktorAlferov <alferov1322@gmail.com>
2017-04-25 13:34:22 +0700

    4 commit

--- generation_3.py
+++ generation_3.py
@@ -11,13 +11,13 @@ Description = collections.namedtuple("Description",
 def main():
 
     if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:
-        print("usage: {0} file1 [file2 [... fileN]]".format(sys.argv[0])) 
+        print("usage: {0} file1 [file2 [... fileN]]".format(sys.argv[0]))
         sys.exit()
     filenames = sys.argv[1:]
     descriptions = create_discriptions(filenames)
     save_discriptions(descriptions)
     print(descriptions)
-    
+
 
 def create_discriptions(filenames):
     descriptions = {}
@@ -25,24 +25,23 @@ def create_discriptions(filenames):
         for line in open(filename, encoding="utf8"):
             line = line.rstrip()
             if line:
-                fields = line.split(":")
-                description = Description(fields[GENDER], fields[PRODUCT], fields[CLASP_TYPE],
-                                          fields[POCKET_TYPE], fields[SEAM_LENGHT], fields[CUTTING],
-                                          fields[DESIGN_EFFECTS], fields[SEASON], fields[ID])
-
-            descriptions[(description.id)] = description
+                description = process_line(line)
+                descriptions[(description.id)] = description
     return descriptions
 
+def process_line(line):
+    fields = line.split(":")
+    description = Description(fields[GENDER], fields[PRODUCT], fields[CLASP_TYPE],
+                              fields[POCKET_TYPE], fields[SEAM_LENGHT], fields[CUTTING],
+                              fields[DESIGN_EFFECTS], fields[SEASON], fields[ID])
+    return description
 
 def save_discriptions(descriptions):
-
-
     for key in sorted(descriptions):
-        
         descript = descriptions[key]
         with open('data/descriptions_out.txt', 'a') as file:
-            
-            print("{0:.5}ие {1} на {2:.7}ах декорированные {3:.6}ми карманами." 
+
+            print("{0:.5}ие {1} на {2:.7}ах декорированные {3:.6}ми карманами."
                   "Длинные брючины {4}. Крой типа {5} позволяет подчеркнуть вашу фигуру,"
                   "а эффект {6} создает небрежный образ."
                   "Подходит на {7} сезон.:{8}".format(descript.gender, descript.product, descript.clas_type,
@@ -54,6 +53,5 @@ main()
 
 #todo без сортировки итерироваться
 #todo с сортировкой
-# format вместо print
-# 24 на with
+# format вместе с print
 # create_description на две функции
